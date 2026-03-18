efca21ce0fac3afbe077bc6b4f96949b0f97d395
Nivetha B <nivethabaskarraj@gmail.com>
2026-03-18 10:06:53 +0530

    bubble sort problems

---
+++ BubbleSort.py
@@ -0,0 +1,19 @@
+def bubble_sort(array):
+    swapped = False
+    for i in range(number):
+        print(f'current iteration is {i + 1}')
+        for j in range(0, number - i - 1):
+            if array[j] > array[j + 1]:
+                array[j], array[j + 1] = array[j + 1], array[j]
+                swapped = True
+                print(f'modified array is {array}')
+        if not swapped:
+            break
+    return array
+
+number = int(input('number of array elements: '))
+array = []
+for i in range(number):
+    element = int(input(f'enter the element {i + 1}:'))
+    array.append(element)
+print(bubble_sort(array))
---
+++ Reverse_BubbleSort.py
@@ -0,0 +1,20 @@
+def reverse_bubble_sort(array):
+    swapped = False
+    for i in range(number):
+        print(f'current iteration is {i + 1}')
+        for j in range(0, number - i - 1):
+            if array[j] < array[j + 1]:
+                array[j], array[j + 1] = array[j + 1], array[j]
+                swapped = True
+                print(f'modified array is {array}')
+        if not swapped:
+            break
+    return array
+
+number = int(input('number of array elements: '))
+array = []
+for i in range(number):
+    element = int(input(f'enter the element {i + 1}:'))
+    array.append(element)
+result = reverse_bubble_sort(array)
+print(result)
